9a4c97fab89d1e2281a8da543245e700589dc849
Håvard Vågstøl <havvag@scantrolas.local>
2019-03-04 10:29:56 +0100

    fixed val data set

--- ex1/plankton.py
+++ ex1/plankton.py
@@ -70,7 +70,11 @@ le.fit(np.unique(downsampled['taxon']))
 lb.fit(le.transform(np.unique(downsampled['taxon'])))
 
 # split into training and test data
-X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=0.6, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=0.9, random_state=42, stratify=y)
+
+# split training set into training and validation data
+X_train, X_val, y_train, y_val = train_test_split(X_test,y_test,train_size=8/9, random_state=42, stratify=y)
+
 
 # make sure the memory is not clogged with previous data
 K.clear_session()
@@ -117,7 +121,10 @@ optimizer=optimizers.SGD(momentum=0.9, nesterov=True)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy',metrics=['accuracy'])
 
 training_generator = DataGenerator(X_train, y_train, le, lb, **params)
+
+# error here, need to split the training data manually to get X_val, y_val
 validation_generator = DataGenerator(X_test, y_test, le, lb, **params)
+
 testing_generator = DataGenerator(X_test, y_test, le, lb, testing=True, **params_test)
 
 # train the model on the new data for a few epochs
